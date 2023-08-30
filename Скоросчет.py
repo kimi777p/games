5a4933a5971043fbc73067eb786d798b9f11c9f1
kimi777p <143586698+kimi777p@users.noreply.github.com>
2023-08-30 11:40:15 +0300

    Update Скоросчет.py

--- Скоросчет.py
+++ Скоросчет.py
@@ -1,8 +1,32 @@
-from tkinter import *
-import time
+import datetime
+import random
 
-print('stargame скоросчет реши как можно больше примеров за 5 мин')
+coins = 0
+application = 1
+time = datetime.datetime.now()
 
 true = True
 while true:
-    print(
+    var1 = random.randint(0, 10)
+    var2 = random.randint(0, 10)
+    application = random.randint(1, 2)
+    if application == 1:
+        result = input(var1+'+'+var2)
+        if result == format(var1 + var2):
+            print('yes')
+            coins += 1
+        else:
+            print('no')
+            coins -= 1
+    if application == 2:
+        result = input(var1+'-'+var2)
+        if result == format(var1 - var2):
+            print('yes')
+            coins += 1
+        else:
+            print('no')
+            coins -= 1
+    print('your count: '+coins)
+    exit = input('if you want to exit, press(enter), else press(space)')
+    if exit == '':
+            true = False
